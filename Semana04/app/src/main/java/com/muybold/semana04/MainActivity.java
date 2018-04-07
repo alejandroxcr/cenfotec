d9e96ce572e19b52c2406f6f26a7ebc5a09cc0d3
Alejandro <alejandroxcr@gmail.com>
2018-04-06 19:54:46 -0600

    Cambios para mostrar personajes de starwars en listview

--- Semana04/app/src/main/java/com/muybold/semana04/MainActivity.java
+++ Semana04/app/src/main/java/com/muybold/semana04/MainActivity.java
@@ -3,12 +3,16 @@ package com.muybold.semana04;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.muybold.semana04.adapters.PeopleAdapter;
 import com.muybold.semana04.classes.People;
 import com.muybold.semana04.classes.TheForceResponse;
 import com.muybold.semana04.services.StarWarsServiceHandler;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,6 +21,8 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
 
+    @BindView(R.id.people_list)
+    ListView _peopleList;
 
     private TheForceResponse<People> _people;
 
@@ -45,10 +51,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<TheForceResponse<People>> call, Response<TheForceResponse<People>> response) {
 
-                if(response != null)
-                    _people = response.body();
-                else
-                    Toast.makeText(MainActivity.this, getText(R.string.app_emtpy_response), Toast.LENGTH_LONG);
+                try {
+                    if (response != null) {
+
+                        _people = response.body();
+                        populateList();
+
+                    } else {
+
+                        Toast.makeText(MainActivity.this, getText(R.string.app_emtpy_response), Toast.LENGTH_LONG);
+
+                    }
+                }catch (Exception ex){
+
+                    Toast.makeText(MainActivity.this, ex.getMessage(), Toast.LENGTH_LONG);
+
+                }
             }
 
             @Override
@@ -59,6 +77,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    /**
+     * Llena list view con información
+     * del Api de Star Wars
+     */
+    private void populateList(){
+
+        try {
+
+            PeopleAdapter adapter = new PeopleAdapter(this, _people.results);
+            _peopleList.setAdapter(adapter);
+
+        }catch (Exception ex){
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG);
+            finish();
+        }
     }
 
 
---
+++ Semana04/app/src/main/java/com/muybold/semana04/adapters/PeopleAdapter.java
@@ -0,0 +1,70 @@
+package com.muybold.semana04.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.muybold.semana04.R;
+import com.muybold.semana04.classes.People;
+
+
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Usuario on 4/6/2018.
+ */
+
+public class PeopleAdapter extends BaseAdapter {
+
+    private Context _context;
+    private LayoutInflater _inflater;
+    private List<People> _list;
+
+    public PeopleAdapter(Context context, List<People> list){
+
+        _context = context;
+        _list = list;
+        _inflater = (LayoutInflater) _context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return _list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return _list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+
+        View rowView = _inflater.inflate(R.layout.row_people_layout, viewGroup, false);
+
+        try {
+            TextView name = rowView.findViewById(R.id.name);
+            People current = _list.get(position);
+
+            if (current != null)
+                name.setText(current.name);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        return rowView;
+    }
+} // PeopleAdapter
